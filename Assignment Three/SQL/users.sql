33f430e365d4a4aa62ae61b725c4923ceba727b3
Nazmul Hassan <nazmul.phpdroidx@gmail.com>
2020-01-13 09:09:47 +0600

    assignment one, three, four done

---
+++ Assignment Three/SQL/users.sql
@@ -0,0 +1,27 @@
+-- https://paiza.io/projects/_ITVhWzSewy0bmoslQSqHg?language=mysql
+
+create table Users
+(
+  id integer,
+  name varchar(100),
+  occupation varchar(100)
+);
+insert into Users
+  (id, name, occupation)
+values
+  ('1', 'Rifat', 'Doctor'),
+  ('2', 'Samantha', 'Engineer'),
+  ('3', 'Soumya', 'Doctor'),
+  ('4', 'Cristina', 'Economist');
+
+-- Print the names with first letter
+select name, left(name, 1)
+from Users;
+
+-- Print {occupation_count}, occupation SQL
+select
+  count(occupation),
+  occupation
+from Users
+group by
+  occupation;
